ed47324a00ec61c9d5956ad92324effb233d38cd
HanuszczakDev_Mac <adam.hanuszczak@gmail.com>
2023-05-04 13:11:24 +0200

    KtorTranslateClient implementation in shared logic

--- shared/src/commonMain/kotlin/com/hanuszczak/translatorappkmm/translate/data/translate/KtorTranslateClient.kt
+++ shared/src/commonMain/kotlin/com/hanuszczak/translatorappkmm/translate/data/translate/KtorTranslateClient.kt
@@ -1,4 +1,53 @@
 package com.hanuszczak.translatorappkmm.translate.data.translate
 
-class KtorTranslateClient {
+import com.hanuszczak.translatorappkmm.NetworkConstants
+import com.hanuszczak.translatorappkmm.core.domain.language.Language
+import com.hanuszczak.translatorappkmm.translate.doamin.translate.TranslateClient
+import com.hanuszczak.translatorappkmm.translate.doamin.translate.TranslateError
+import com.hanuszczak.translatorappkmm.translate.doamin.translate.TranslateException
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.utils.io.errors.*
+
+class KtorTranslateClient(
+    private val httpClient: HttpClient
+): TranslateClient {
+
+    override suspend fun translate(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): String {
+        val result = try {
+            httpClient.post {
+                url(NetworkConstants.BASE_URL + "/translate")
+                contentType(ContentType.Application.Json)
+                setBody(
+                    TranslateDto(
+                        textToTranslate = fromText,
+                        sourceLanguageCode = fromLanguage.langCode,
+                        targetLanguageCode = toLanguage.langCode
+                    )
+                )
+            }
+        } catch (e: IOException) {
+            throw TranslateException(TranslateError.SERVICE_UNAVAILABLE)
+        }
+
+        when(result.status.value) {
+            in 200..299 -> Unit
+            in 400..499 -> throw TranslateException(TranslateError.CLIENT_ERROR)
+            500 -> throw TranslateException(TranslateError.SERVER_ERROR)
+            else -> throw TranslateException(TranslateError.UNKNOWN_ERROR)
+        }
+
+        return try {
+            result.body<TranslatedDto>().translatedText
+        } catch (e: Exception) {
+            throw TranslateException(TranslateError.SERVER_ERROR)
+        }
+    }
+
 }
